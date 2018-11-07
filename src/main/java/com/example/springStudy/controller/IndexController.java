1c45282af2ae1cc32739eaf78c3b4c47ce084851
liuyingzi <lyzjxust@qq.com>
2018-11-07 14:19:41 +0800

    http://localhost:8080/ http://localhost:8080/index.html 路径访问html页面 同时设置了静态访问路径

---
+++ src/main/java/com/example/springStudy/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.springStudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        System.out.println(".......index........");
+        return "index.html";
+    }
+}
--- src/test/java/com/example/springStudy/SpringStudyApplicationTests.java
+++ src/test/java/com/example/springStudy/SpringStudyApplicationTests.java
@@ -2,8 +2,13 @@ package com.example.springStudy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +18,12 @@ public class SpringStudyApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Value("${web.upload-path}")
+	private String path;
+
+	@Test
+	public void  upload() throws IOException {
+		File f = new File("D:\\code\\github\\springStudy\\README.md");
+		FileCopyUtils.copy(f,new File(path+"\\read.md"));
+	}
 }
